399330262084797e0aa7b14f38bdccf39e3e9121
ibnukhamalz <ibnukhamalz@gmail.com>
2022-11-12 10:56:34 +0700

    Revert "firstcomit"
    This reverts commit c0bb2f344fa76c088684eda6d292d12494cb3562.

--- template/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-var primary = localStorage.getItem("primary") || '#7366ff';
-var secondary = localStorage.getItem("secondary") || '#f73164';
-
-window.CubaAdminConfig = {
-	primary: primary,
-	secondary: secondary,
-};
